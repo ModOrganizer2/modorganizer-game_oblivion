ff7f6cc7c5fffb43a5169453826d11a212d9f1cd
Mikaël Capelle <capelle.mikael@gmail.com>
2020-05-23 11:39:34 +0200

    Use a dedicated ModDataChecker.

---
+++ src/oblivionmoddatachecker.h
@@ -0,0 +1,31 @@
+#ifndef OBLIVION_MODATACHECKER_H
+#define OBLIVION_MODATACHECKER_H
+
+#include <gamebryomoddatachecker.h>
+
+class SkyrimSEModDataChecker : public GamebryoModDataChecker
+{
+public:
+  using GamebryoModDataChecker::GamebryoModDataChecker;
+
+protected:
+  virtual const FileNameSet& possibleFolderNames() const override {
+    static FileNameSet result{
+      "fonts", "interface", "menus", "meshes", "music", "scripts", "shaders",
+      "sound", "strings", "textures", "trees", "video", "facegen",
+      "obse", "distantlod", "asi", "distantland", "mits", "dllplugins", "CalienteTools", 
+      "NetScriptFramework"
+    };
+    return result;
+  }
+  virtual const FileNameSet& possibleFileExtensions() const override {
+    static FileNameSet result{
+      "esp", "esm", "bsa", ".modgroups"
+    };
+    return result;
+  }
+
+  const GameGamebryo* m_Game;
+};
+
+#endif // OBLIVION_MODATACHECKER_H

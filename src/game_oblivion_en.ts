c4589dd80a89b23aff9501c845991dacb3e454b5
Mikaël Capelle <capelle.mikael@gmail.com>
2024-06-22 08:29:45 +0200

    Update following gamebryo updates for myGamesPath() in game feature. (#25)

--- src/game_oblivion_en.ts
+++ src/game_oblivion_en.ts
@@ -4,12 +4,12 @@
 <context>
     <name>GameOblivion</name>
     <message>
-        <location filename="gameoblivion.cpp" line="80"/>
+        <location filename="gameoblivion.cpp" line="84"/>
         <source>Oblivion Support Plugin</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gameoblivion.cpp" line="90"/>
+        <location filename="gameoblivion.cpp" line="94"/>
         <source>Adds support for the game Oblivion</source>
         <translation type="unfinished"></translation>
     </message>
